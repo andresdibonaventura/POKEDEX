18d1eae5de3185bf4fb1d5b1e793ffaa724aaeb5
Your Name <you@example.com>
2022-07-01 00:14:28 -0400

    ya

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import { Route, Routes } from 'react-router-dom'
 import PokedexScreen from './components/home/pokedex/PokedexScreen'
 import HomeScreen from './components/home/HomeScreen'
 import PokeInfo from './components/home/pokedex/pokeInfo/PokeInfo'
-import PokeType from './components/home/pokedex/PokeType'
+
 import PokeEle from './components/home/PokeEle'
-import Login from './components/ProtectRoutes/login'
+
 import { Switch } from 'antd'
 
 
@@ -29,7 +29,7 @@ function App() {
       <Routes>
         
         <Route path='/' element={<HomeScreen setLogged={setLogged}/>} />
-        <Route element={<Login logged={logged}/>}/>
+       
         <Route className='head' path='/pokedex' element={<PokedexScreen />} />
         <Route path='/pokedex/:id' element={<PokeInfo />} />
         {/* <Route  path='/pokedex/:id' element={<PokeType /> }/> */}
