aa252bd79d63e61ac09a570163c7e0b9e73ad3b9
Your Name <you@example.com>
2022-07-01 00:28:13 -0400

    aa

--- src/components/home/pokedex/PokedexScreen.jsx
+++ src/components/home/pokedex/PokedexScreen.jsx
@@ -35,7 +35,7 @@ const PokedexScreen = () => {
     <div className='header'>
       <div className='headerCircle'>
       </div>
-      <h1><img src="https://archives.bulbagarden.net/media/upload/thumb/4/4b/Pok%C3%A9dex_logo.png/250px-Pok%C3%A9dex_logo.png" alt="" /></h1>
+      <h1 className='pokedexx'><img src="https://raw.githubusercontent.com/sleduardo20/pokedex/0671af442dff1d8f7141e49eb83b438885bbc9e9/public/img/logo.svg" alt="" /></h1>
       <h2 className='margiTop margiLeft'>Hola {nameUser}, bienvenido a la pokedex </h2>
       <h3 className='margiTop'><PokeType pokemons={pokemons} setfilterPokemon={setfilterPokemon}/></h3>
         
