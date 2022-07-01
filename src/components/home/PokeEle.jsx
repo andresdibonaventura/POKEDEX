4727db5c5ca176882fccc2d138efb8cb81c4e980
Your Name <you@example.com>
2022-07-01 00:31:42 -0400

    listo

--- src/components/home/PokeEle.jsx
+++ src/components/home/PokeEle.jsx
@@ -20,7 +20,7 @@ const PokeEle = () => {
     
     <div className='header'>
       <div className='headerCircle'></div>
-      <h1 className='pokedexx'><img src="https://raw.githubusercontent.com/sleduardo20/pokedex/0671af442dff1d8f7141e49eb83b438885bbc9e9/public/img/logo.svg" alt="" /></h1>
+      <h1><img src="https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png" alt="" /></h1>
     
       
      
